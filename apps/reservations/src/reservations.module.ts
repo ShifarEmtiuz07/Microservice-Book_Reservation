85e69732bdf7bf8276567761d8975d8dcb47ec67
A. B. M. Shifar Emtiuz <emitiazshifar007@gmail.com>
2025-03-06 16:03:00 +0600

    06.03.25

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,19 +3,28 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
-import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports:[
+  imports: [
     DatabaseModule,
-    DatabaseModule.forFeature([{name:ReservationDocument.name, schema:ReservationSchema},
-
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
- 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI:Joi.string().required() ,
+      }),
+    }),
   ],
   controllers: [ReservationsController],
-  providers: [ReservationsService,ReservationsRepository ],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
